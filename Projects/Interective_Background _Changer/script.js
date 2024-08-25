836ccfe92fa621a90f4adb80ba215bb29d8d6097
Arin <arinmandal40@gmail.com>
2024-08-26 00:24:14 +0530

    DOM Mini Project

---
+++ Projects/Interective_Background _Changer/script.js
@@ -0,0 +1,25 @@
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+let orange = document.getElementById('orange');
+let crimson = document.getElementById('crimson');
+
+red.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'red';
+});
+
+green.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'green';
+});
+
+blue.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'blue';
+});
+
+orange.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'orange';
+});
+
+crimson.addEventListener('click', function() {
+  document.body.style.backgroundColor = 'crimson';
+});
