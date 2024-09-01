515ccc15ccb9c79f74bb6c860a124158ddd3801a
Arin <arinmandal40@gmail.com>
2024-09-01 23:59:33 +0530

    Day13~ React

---
+++ Day13~FrontEnd/React-first/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a basic React component.</p>
+    </div>
+  );
+}
+
+export default App;
