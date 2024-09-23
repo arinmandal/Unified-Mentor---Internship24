65540b0c70bb2bfcde0c10e3279d865dc5a73be6
Arin <arinmandal40@gmail.com>
2024-09-23 20:53:46 +0530

    image-cdn: Convert default export to named export to improve readability

--- Projects/DineDirect/src/Utils/constantFile.js
+++ Projects/DineDirect/src/Utils/constantFile.js
@@ -1,4 +1,12 @@
-const IMAGE_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+// Restaurant Images 
+export const IMAGE_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
+// Hero Background
 export const HERO_IMAGE = "https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-export default IMAGE_CDN;
+
+// Restaurant List API
+export const RESTAURANT_API = "https://www.swiggy.com/mapi/homepage/getCards?lat=22.5743545&lng=88.3628734"
+
+// Restaurant Menu API
+
+export const getMenuApi= (resId) => `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=${resId}`
