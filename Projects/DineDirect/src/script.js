55aeead0e240963883cca60d7a69b1e7ad6ae358
Arin <arinmandal40@gmail.com>
2024-09-23 20:39:56 +0530

    updated with routing

--- Projects/DineDirect/src/script.js
+++ Projects/DineDirect/src/script.js
@@ -1,8 +1,41 @@
 import './index.css';
-import React from 'react';
+import { SignIn } from './Pages/SignIn';
+import HelpPage from './Pages/HelpPage';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Main } from './components/Main';
+import RestaurantMenu from './components/RestaurantMenu';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Main />
+      },
+      {
+        path: '/help',
+        element: <HelpPage />
+      },
+      {
+        path: '/signIn',
+        element: <SignIn />
+      },
+      {
+        path: '/restaurants/:resId',
+        element: <RestaurantMenu/>  
+      }
+
+    ],
+    errorElement: <Error />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
