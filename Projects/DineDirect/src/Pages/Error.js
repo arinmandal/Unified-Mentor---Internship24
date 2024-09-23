b6eb5316bae3d398f5bb654a9bd4208fa391e4c9
Arin <arinmandal40@gmail.com>
2024-09-23 20:45:39 +0530

    Added GO HOME button

--- Projects/DineDirect/src/Pages/Error.js
+++ Projects/DineDirect/src/Pages/Error.js
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const ErrorComponent = () => {
+
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="container mx-auto px-4">
@@ -9,8 +11,9 @@ const ErrorComponent = () => {
           <p className="text-lg text-gray-600">
             We're sorry, but there seems to be an error. Please try again later.
           </p>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline">
-            Reload Page
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus-shadow-outline"
+            onClick={() => navigate('/')}>
+            Go Home
           </button>
         </div>
       </div>
