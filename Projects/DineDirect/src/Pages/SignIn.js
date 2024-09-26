137cf6b6fd3b829e867175bdd3f8ca3e571d9b0a
Arin <arinmandal40@gmail.com>
2024-09-26 20:12:39 +0530

    Remove unwanted code

--- Projects/DineDirect/src/Pages/SignIn.js
+++ Projects/DineDirect/src/Pages/SignIn.js
@@ -3,14 +3,6 @@ export const SignIn = () => {
   const navigate = useNavigate()
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-white">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8 dark:bg-gray-500">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h1
