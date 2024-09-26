75cc3becd4810cc93254929e4db40c03b39974b4
Arin <arinmandal40@gmail.com>
2024-09-26 20:15:07 +0530

    Update restaurant API URL

--- Projects/DineDirect/src/Utils/constantFile.js
+++ Projects/DineDirect/src/Utils/constantFile.js
@@ -5,8 +5,7 @@ export const IMAGE_CDN = "https://media-assets.swiggy.com/swiggy/image/upload/fl
 export const HERO_IMAGE = "https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
 // Restaurant List API
-export const RESTAURANT_API = "https://www.swiggy.com/mapi/homepage/getCards?lat=22.5743545&lng=88.3628734"
+export const RESTAURANT_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5743545&lng=88.3628734"
 
 // Restaurant Menu API
-
-export const getMenuApi= (resId) => `https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=${resId}`
+export const getMenuApi = (resId) => `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5743545&lng=88.3628734&restaurantId=${resId}`
