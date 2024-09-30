a75cd4d27147c4bdeec8ff79abd3f9b3f3e9dafb
Arin <arinmandal40@gmail.com>
2024-10-01 00:18:49 +0530

    Update App component

--- Projects/DineDirect/src/components/App.jsx
+++ Projects/DineDirect/src/components/App.jsx
@@ -1,12 +1,16 @@
 import { Header } from "./Header";
 import { Main } from "./Main";
 import { Outlet } from "react-router-dom";
+import appStore from "../ReduxStore/appStore";
+import { Provider } from "react-redux";
 const App = () => {
   return (
-    <div className='App bg-mainBg'>
-      <Header />
-      <Outlet />
-    </div>
+    <Provider store={appStore}>
+      <div className='App bg-mainBg'>
+        <Header />
+        <Outlet />
+      </div>
+    </Provider>
   );
 };
 
