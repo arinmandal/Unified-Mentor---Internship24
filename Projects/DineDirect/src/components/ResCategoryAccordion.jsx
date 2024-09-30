4ceb5718f04acb0f51b1f8d4b2191d91189ac7c8
Arin <arinmandal40@gmail.com>
2024-10-01 00:48:15 +0530

    Updated accordion

--- Projects/DineDirect/src/components/ResCategoryAccordion.jsx
+++ Projects/DineDirect/src/components/ResCategoryAccordion.jsx
@@ -1,21 +1,22 @@
-import MenuItems from "./menuItems";
-import { useState } from "react";
-const ResCategoryAccordion = ({ data }) => {
-  const [isActive, setIsActive] = useState(false);
-
+import MenuItems from "./MenuItems";
+const ResCategoryAccordion = ({ data, showItems, setShowIndex }) => {
   const { title, itemCards } = data;
+
+  const toggleMenu = () => {
+    setShowIndex();
+  };
   return (
     <div className='bg-white rounded-lg shadow-md p-4 mb-4'>
       <div
-        className='header flex justify-between '
-        onClick={() => setIsActive(!isActive)}
+        className='header flex justify-between cursor-pointer'
+        onClick={toggleMenu}
       >
         <span className='text-lg font-semibold'>
           {title}({itemCards.length})
         </span>
-        <span>{isActive ? "▲" : "▼"}</span>
+        <span>{showItems? "▲" : "▼"}</span>
       </div>
-      {isActive && <MenuItems items={data.itemCards} />}
+      {showItems && <MenuItems items={itemCards} />}
     </div>
   );
 };
